f3d74d66818ac75497d7062dcff84687342f506f
TianruiLiu <tliu1@macalester.edu>
2019-06-27 16:03:22 -0500

    improved functions find_smallest and sum_of_all_counts to make them faster

--- bin/taxonomyFilter.py
+++ bin/taxonomyFilter.py
@@ -32,11 +32,11 @@ def original_num_of_samples(data):
 
 
 def find_smallest(list):
-    smallest = list[1]
-    for i in range(1, len(list)):
-        if list[i] < smallest:
-            smallest = list[i]
-    return smallest
+    list2 = list.copy()
+    if list2[0] == 'Total Counts':
+        list2.remove('Total Counts')
+    list2.sort()
+    return list2[0]
 
 
 def find_smallest_index(list):
@@ -50,18 +50,15 @@ def find_smallest_index(list):
 
 
 def sum_of_all_counts(data):
-    count = 0
-    for i in range(1, len(csv_to_array(data)) - 1):
-        for h in range(1, len(csv_to_array(data)[i])):
-            count = count + int(csv_to_array(data)[i][h])
-    return count
+    df = import_data(data)
+    df = df.drop([df.columns[0]], axis=1)
+    return df.values.sum()
 
 
 def otu_processor(data):
-    x = int(input("Enter an integer to filter OTUs that have low total counts across the dataset: "))
-    y = float(input("Enter a float to filter OTUs that are present in onlt a small proportion of the samples: "))
+    x = int(input("Enter an integer to filter OTUs that have low total counts across the data set: "))
+    y = float(input("Enter a float to filter OTUs that are present in only a small proportion of the samples: "))
     df = import_data(data)
-    print(csv_to_array(data))
     for i in range(1, len(csv_to_array(data))):
         sum_of_row = 0
         num_of_non0 = 0
@@ -85,7 +82,7 @@ def sample_processor(df):
     new_value_list = list()
     new_index_list.append(index_list[0])
     new_value_list.append(value_list[0])
-    for i in range(0,len(index_list)-1):
+    for i in range(0, len(index_list)-1):
         smallest = find_smallest(value_list)
         smallest_index = find_smallest_index(value_list)
         new_value_list.append(smallest)
@@ -125,7 +122,6 @@ def processor():
 def delete_column(metadata, list):
     df = import_data(metadata)
     df = df.drop(list, axis=0)
-    print(df)
     return df
 
 
@@ -141,9 +137,9 @@ def processor_with_metadata():
     print("The final number of OTUs is: " + str(final_OTUs))
     print("The final number of samples is: " + str(final_samples))
     output_path1 = input("Enter a path to save the OTU table: ")
-    df_after_both_filter.to_csv(output_path1, sep=' ')
+    df_after_both_filter.to_csv(output_path1, sep='\t', index=False)
     output_path2 = input("Enter a path to save the metadata file: ")
-    df_metadata_after_sample_filter.to_csv(output_path2, sep=' ')
+    df_metadata_after_sample_filter.to_csv(output_path2, sep='\t', index=False)
 
 
 if __name__ == '__main__':
